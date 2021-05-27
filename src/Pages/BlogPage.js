f8d04a7a84ad250a061e353d948c7af3e5f06e6a
Johan García Trejo <johan.garcia.uicslp.tmz@gmail.com>
2021-05-26 23:39:00 -0500

    feat: :sparkles: Diseño de BlogPage
    Se codifica BlogPage, para mostrar los blogs mas recientes

--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Title from "../Components/Title";
+import allBlogs from "../Data/allBlogs";
 
 function BlogPage() {
   return (
     <div className="BlogPage">
-      <h1>Blog Page</h1>
+      <Title title={"Recent Blogs"} span={"Recent Blogs"} />
+      <div className="blog-container">
+        {allBlogs.map((blog) => {
+          return (
+            <div className="blog" key={blog.id}>
+              <div className="blog-content">
+                <img src={blog.image} alt={blog.title} />
+                <a href={blog.link} className="blog-link">
+                  {blog.title}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
